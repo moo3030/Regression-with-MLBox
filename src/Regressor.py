f0623ad0adcf09c94dbc75d279e3ad4c68e14f67
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-10-05 17:01:55 +0300

    edit config file

--- src/Regressor.py
+++ src/Regressor.py
@@ -100,7 +100,7 @@ class Regressor:
                 'est__max_depth': self.model_config["est__max_depth"],
                 'est__n_estimators': self.model_config["est__n_estimators"],
                 'est__num_leaves': self.model_config["est__num_leaves"],  # specific to LightGBM,
-                'est__n_jobs': {"search": "choice", "space": [1]},
+                'est__n_jobs': {"search": "choice", "space": [self.model_config["n_jobs"]]},
                 'ne__numerical_strategy': {"search": "choice", "space": ["median"]},
                 'ce__strategy': {"search": "choice", "space": ["dummification"]},
             },
